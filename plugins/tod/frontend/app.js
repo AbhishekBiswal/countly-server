004db21eb1298dc2ac706942e54b824e07854e7d
Abhishek Biswal <wirddin@gmail.com>
2017-07-01 02:52:47 +0530

    Route for tod + render d3 with dummy data

--- plugins/tod/frontend/app.js
+++ plugins/tod/frontend/app.js
@@ -1,8 +1,23 @@
-var plugin = {};
+var plugin = {},
+countlyConfig = require('../../../frontend/express/config');
 
 (function (plugin) {
 	plugin.init = function(app, countlyDb){
-		
+		app.get("/o", function(req, res, next) {
+			res.send("Hey");
+			// Connect to DB
+			countlyDb.collection('tod').find({}, function(err, plugindata){
+				if (err) res.send('404: Page not Found', 404);
+				else {
+							//render template with data	
+		 			res.render('../../../plugins/tod/frontend/public/templates/default', {
+						path:countlyConfig.path || "", 
+						cdn:countlyConfig.cdn || "", 
+						data:plugindata
+		        	});
+				}
+			});
+		});
 	};
 }(plugin));
 
